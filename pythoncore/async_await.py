3a122908b443e962a64e0e955ed2cf856e3f285c
tom <email@example.com>
2018-02-08 13:32:36 +0800

    PEP 525

--- pythoncore/async_await.py
+++ pythoncore/async_await.py
@@ -95,3 +95,39 @@ async def main():
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
+
+# %%
+import asyncio
+import types
+
+@types.coroutine
+def compute(x, y):
+    print("Compute %s + %s ..." % (x, y))
+    yield from asyncio.sleep(1.0)
+
+async def print_sum(x, y):
+    result = await compute(x, y)
+    print("%s + %s = %s" % (x, y, result))
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(print_sum(1, 2))
+loop.close()
+
+# %%
+import asyncio
+
+async def async_generator():
+    for i in range(3):
+        await asyncio.sleep(1)
+        yield i*i
+
+async def main():
+    async for i in async_generator():
+        print(i)
+
+loop = asyncio.get_event_loop()
+try:
+    loop.run_until_complete(main())
+finally:
+    loop.run_until_complete(loop.shutdown_asyncgens())  # see: https://docs.python.org/3/library/asyncio-eventloop.html#asyncio.AbstractEventLoop.shutdown_asyncgens
+    loop.close()
